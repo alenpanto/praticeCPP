495795309743e3c8994a43c8ce3636af9d46673d
alenpanto <137296998+alenpanto@users.noreply.github.com>
2023-06-21 06:07:19 -0700

    nested loop

---
+++ nexted loop 3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<< "enter a row:";
+    cin>>n;
+
+    for (int i=1; i<=n; i++){
+            for(int k=n-i;k>0; k--){
+            cout<<" ";
+
+    }
+        for (int j=1; j<=i; j++){
+
+            cout<< "*";
+        }
+        cout<< "\n";
+    }
+    return 0;
+
+}
