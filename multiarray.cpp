f9c27a1b17a17f732cce5a7d743f7c94f3f378a0
ALEN P ANTO <137296998+alenpanto@users.noreply.github.com>
2023-06-26 06:16:04 -0700

    multiarray

--- multiarray.cpp
+++ multiarray.cpp
@@ -2,9 +2,15 @@
 using namespace std;
 int main()
 {
-    string a[2][4]={
+     string a[2][4] ={
         {"as","dc","fg","hj"},
         {"jj","dc","fg","hj"}
         };
-    cout<<a[1][0];
-}
+      string iteam =a[0][2];
+      cout<<iteam;
+     a[1][3] = "bmw";
+      cout<<a[1][3];
+     
+  
+    
+} 
