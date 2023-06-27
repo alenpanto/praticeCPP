add812b28feef6ee522c8d191ab0f021e908bc9b
ALEN P ANTO <137296998+alenpanto@users.noreply.github.com>
2023-06-27 06:27:53 -0700

    multiarray.cpp

--- multiarray.cpp
+++ multiarray.cpp
@@ -6,10 +6,24 @@ int main()
         {"as","dc","fg","hj"},
         {"jj","dc","fg","hj"}
         };
-      string iteam =a[0][2];
-      cout<<iteam;
-     a[1][3] = "bmw";
-      cout<<a[1][3];
+    //   string iteam =a[0][2];
+    //   cout<<iteam;
+    //  a[1][3] = "bmw";
+    //   cout<<a[1][3];
+    //   cout<<a[2][4]<< "\n";
+
+      for ( int i=0;i<=2;i++)
+      {
+        for(int j=0;j<=4;j++)
+        {
+          //cout<<i<< " "<<j;
+          cout<<a[i][j];
+
+          
+        }
+        cout<< "\n";
+      }
+
      
   
     
