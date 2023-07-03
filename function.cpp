f1bf11b98f80416b49de706c773cc9f9ead08e2f
ALEN P ANTO <137296998+alenpanto@users.noreply.github.com>
2023-07-03 06:05:37 -0700

    function

---
+++ function.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void demo()
+{
+    cout<<"hai"<< "\n";
+}
+void greetings()
+{
+    cout<<"good afternoon";
+}
+void introduction(string name,int age)
+{
+    cout<<" \n I AM "<<name;
+    cout<< "\n i am "<< age<< "years old \n";
+    
+
+}
+ 
+ 
+int main()
+{
+   demo();
+   greetings();
+   introduction("alen",17);
+   return 0;
+}
+
+
