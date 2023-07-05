49194524c697ac24b89876b2c108b6eee2179133
ALEN P ANTO <137296998+alenpanto@users.noreply.github.com>
2023-07-05 05:53:46 -0700

    c++

---
+++ functionoverloading.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void add(int a,int b)
+{
+    
+    int sum;
+    sum = a+b;
+    cout<<"sum istgtry "<<sum;
+}
+void add(double a,double b)
+{
+    
+    double sum;
+    sum = a+b;
+    cout<<"sum is "<<sum;
+    cout<< "agygygyeuyg  ";
+}
+int main()
+{
+    
+    int n,l;
+    cout<< "enter first number";
+    cin>>n;
+    cout<< "enter second number";
+    cin>>l;
+    add(n,l);
+    
+}
