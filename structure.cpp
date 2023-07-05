ec09316a202a017ed85b3404f23cdd6b651fbde2
ALEN P ANTO <137296998+alenpanto@users.noreply.github.com>
2023-07-05 06:14:36 -0700

    structure1

---
+++ structure.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    struct{
+        int adno;
+        string name;
+          }    person1;
+
+    person1.adno=5;
+    person1.name= "alen";
+    cout<<person1.adno<< "\n"<<person1.name;
+
+       
+}
