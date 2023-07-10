b84f4956a92c18d5339467553d739f804eb78bc9
ALEN P ANTO <137296998+alenpanto@users.noreply.github.com>
2023-07-10 06:04:43 -0700

    c++

---
+++ kyc.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+int main(){
+    struct kyc
+    {
+        string name;
+        string fathername;
+        string gender;
+       string dob;
+        int age;
+
+
+    } person1, person2, person3;
+
+
+    cout<< "enter the name\n";
+    cin>>person1.name;    
+    cout<< "enter the father name \n";
+    cin>>person1.fathername;
+    cout<< "enter the gender \n"; 
+    cin>>person1.gender;  
+    cout<< "enter the age \n";
+    cin>>person1.age; 
+
+    cout<<person1.name<< "\n";
+    cout<<person1.fathername<<"\n";
+    cout<<person1.gender<<"/n";\
+    cout<<person1.dob<<"\n";
+    cout<<person1.age<< "\n";
+
+   cout<< "enter the name\n";
+    cin>>person2.name;    
+    cout<< "enter the father name \n";
+    cin>>person2.fathername;
+    cout<< "enter the gender \n"; 
+    cin>>person2.gender;  
+    cout<< "enter the age \n";
+    cin>>person2.age; 
+
+    cout<<person2.name<< "\n";
+    cout<<person2.fathername<<"\n";
+    cout<<person2.gender<<"/n";\
+    cout<<person2.dob<<"\n";
+    cout<<person2.age<< "\n";
+
+  cout<< "enter the name\n";
+    cin>>person3.name;    
+    cout<< "enter the father name \n";
+    cin>>person3.fathername;
+    cout<< "enter the gender \n"; 
+    cin>>person3.gender;  
+    cout<< "enter the age \n";
+    cin>>person3.age; 
+
+    cout<<person3.name<< "\n";
+    cout<<person3.fathername<<"\n";
+    cout<<person3.gender<<"/n";\
+    cout<<person3.dob<<"\n";
+    cout<<person3.age<< "\n";
+
+
+
+
+}
